2a8003cf01a8d38698a4e846f90b8073423bf3e9
OG-1534 <gershomochanda@gmail.com>
2024-07-14 05:56:21 +0300

    debug

--- 8-index_my_names.sql
+++ 8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Create an index idx_name_first on the table names
 -- and the first letter of name.
-
-CREATE INDEX idx_name_first ON names(name(1));
+DROP INDEX IF EXISTS idx_name_first ON names;
+CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));

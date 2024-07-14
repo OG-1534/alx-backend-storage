3718f05df345fd5b4a07af52406be26ff2697ae3
OG-1534 <gershomochanda@gmail.com>
2024-07-14 05:41:34 +0300

    SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.

--- 10-div.sql
+++ 10-div.sql
@@ -1,13 +1,16 @@
 -- Creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0
 -- if the second number is equal to 0.
-
-DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END
-$$
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
 DELIMITER ;

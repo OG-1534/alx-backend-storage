f9993ab5f7cc7f75bbd000999e58c7c30aac76a6
OG-1534 <gershomochanda@gmail.com>
2024-07-14 06:00:40 +0300

    debug

--- 9-index_name_score.sql
+++ 9-index_name_score.sql
@@ -1,4 +1,5 @@
 -- Creates an index idx_name_first_score
 -- on the table names and the first letter of name
 -- and the score must be indexed.
-CREATE INDEX idx_name_first_score ON names(name(1), score);
+DROP INDEX IF EXISTS idx_name_first_score ON names;
+CREATE INDEX idx_name_first_score ON names (SUBSTRING(nam, 1, 1) score);
